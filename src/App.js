b52d0e3fd157096053ec28c5bdbf9f493998c0bf
steven oakley <stevenoakley211@live.com>
2019-12-13 22:50:13 -0600

    mvp

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import Header from "./components/Header.js";
 import {BrowserRouter as Router,Route,Link} from "react-router-dom"
 import CharacterList from "./components/CharacterList"
 import WelcomePage from './components/WelcomePage';
+import SearchForm from "./components/SearchForm.js";
 export default function App() {
   return (
     <Router>
       <main>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/character-list">Characters</Link>
+        </nav>
         <Header />
-        <Route exact Path="/" components={WelcomePage}/>
+        <Route exact Path="/" component={WelcomePage}/>
+        <Route path="/character-list" component={SearchForm} />
       </main>
     </Router>
   );
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,21 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios"
-import SearchForm from './SearchForm';
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
-  const [characters,setCharacters] = useState([]);
-  useEffect(() => {
-    axios.get( "https://rickandmortyapi.com/api/character/")
-          .then (response =>{
-            setCharacters(response.data.results)
-            console.log(characters)
-          })
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+import CharacterCard from "./CharacterCard";
 
+export default function CharacterList(props) {
+  // TODO: Add useState to track data from useEffect
+console.log("props",props)
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {props.characterList.map(item => {
+        return <CharacterCard name={item.name}imageURL={item.image}gender={item.gender} species={item.species}></CharacterCard>
+      })
+      }
     </section>
   );
 }
